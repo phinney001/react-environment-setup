3908ed275678923c8d0103906eb0e4b9735fd448
phinney <phinney001@qq.com>
2021-06-17 15:37:38 +0800

    🐞 fix(vite版本入口文件): 解决antd日历显示英文问题

--- template/vite/src/main.tsx
+++ template/vite/src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom'
 import RouteAuth from '@/routes/RouteAuth'
 import { StoreProvider } from '@/store'
 import { ConfigProvider } from 'antd'
-import zhCN from 'antd/lib/locale/zh_CN';
+import zhCN from 'antd/lib/locale/zh_CN'
+import 'moment/dist/locale/zh-cn'
 import './main.less'
 
 // 生产开启严格模式
