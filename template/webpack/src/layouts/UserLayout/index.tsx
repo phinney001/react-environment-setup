d2a9cf37f271f38f4eb94f71f34c63e92f28571d
phinney <phinney001@qq.com>
2021-07-14 16:51:05 +0800

    🐞 fix(vite、webpack): 菜单栏滚动缺失菜单bug修复

--- template/webpack/src/layouts/UserLayout/index.tsx
+++ template/webpack/src/layouts/UserLayout/index.tsx
@@ -185,6 +185,7 @@ export default (props: any) => {
         <Menu
           theme="dark"
           mode="inline"
+          className={styles.pageMenu}
           defaultOpenKeys={getDefaultOpenKeys()}
           selectedKeys={[history.location.pathname]}
           onClick={({ key }: any) => {
